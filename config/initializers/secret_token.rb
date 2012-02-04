df4cc87e55a621b34f26556a9e0a2b14cc97b8f7
Wei Lu <luwei.here@gmail.com>
2012-02-04 17:40:47 +0800

    set up gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhenGivenAMovieHardDrive::Application.config.secret_token = 'f60abf4a32005fce8668c646284090017be24d9240aa2845eef921c0faf0af7f970ad7d5789b082cb841d2ba60a0d6fd1083026553d7ba1f44cbd9c7bd671427'
